8d2b9394c383ea57037f68d5c8f97a6c9a65bc4f
Kabina Thapa <thapakabina28@gmail.com>
2023-07-19 13:44:22 -0400

    Create a redux state slice

---
+++ src/redux/todoSlice.tsx
@@ -0,0 +1,24 @@
+import {createSlice} from '@reduxjs/toolkit'
+//import type {PayLoadAction} from '@reduxjs/toolkit'
+export const todoSlice=createSlice({
+    name:'todo',
+    initialState:[{id:1, title:'laundry', completed:false}],
+    //reducers responds to actions, it takes the current state and creates new state based on the action payload.
+    reducers:{
+        //takes current state 
+        //addtodo reducers, state-> is current state of slice, action->contains type and payload that comes from our components.
+        //anytime we call addTodo from our components addTodo reducer will handle the action.
+        addTodo:(state, action)=>{
+            //logic to add
+            const newTodo={ 
+                id:Date.now(),
+                title:action.payload.title,
+                completed:false,
+            }
+            state.push(newTodo)
+
+            } 
+        }
+
+    }
+})
