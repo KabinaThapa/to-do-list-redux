484feca9b43400360c05b9b9ac6c18a9976d0934
Kabina Thapa <thapakabina28@gmail.com>
2023-07-20 14:13:15 -0400

    Complete protected routing

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,10 +6,12 @@ import {Link} from 'react-router-dom'
 const home = () => {
   return (
     <>
-    <div className='bgimg w-full h-screen flex flex-col mt-12 items-center text-lg font-bold'>
-        <h1 className='mb-6'>TodoWorx</h1>
-        <p>Simplify Your Productivity</p>
-        <Link to='/sign-in'>Get Started</Link>
+    <div className='bgimg w-full h-screen flex items-center'>
+      <div className='w-96 h-96 flex flex-col items-center mx-auto text-lg font-medium p-8 text-blue-900'>
+        <h1 className='mb-1 text-3xl font-medium'>TodoWorx</h1>
+        <p className='text-xl'>Simplify Your Productivity</p>
+        <Link to='/sign-in' className='mt-auto text-3xl'><button className='border-2 rounded-md p-2 border-black backdrop-blur-sm'>Get Started</button></Link>
+        </div>
     </div>
     </>
   )
