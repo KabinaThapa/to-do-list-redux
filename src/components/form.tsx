8dc17c2084a8c54cd247eb79f6142c9afa9a929f
Jharanatmg <jharanatmg22@gmail.com>
2023-07-19 18:03:54 -0700

    add todo task

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,89 +1,79 @@
-import React, { ChangeEvent,useState } from 'react'
+import React, { ChangeEvent, useState } from "react";
 
+import { useSelector } from "react-redux";
 
-import {useSelector} from 'react-redux'
-import { RootState } from '../redux/store'
-import {useDispatch} from 'react-redux'
-import {toggleComplete, deleteTodo, ITodo,addTodo,updateTodo } from '../redux/todoSlice' 
+import { RootState } from "../redux/store";
 
+import { useDispatch } from "react-redux";
 
-
+import { ITodo, addTodo, updateTodo } from "../redux/todoSlice";
 
 const form = () => {
-    const dispatch=useDispatch()
-    const todo=useSelector((state:RootState)=>state.todo.todos)
-    
-    const[input, setInput]=useState('')
-    const[editedId, setEditedId]=useState<number|null>(null)
-  
-    //inputhandler function
-    const handleInput=(event:ChangeEvent<HTMLInputElement>)=>{
-        setInput(event.target.value)
+  const dispatch = useDispatch();
 
-    }
-   
-   
-    const handleToogle=(id:number)=>{
-        dispatch(toggleComplete(id))
-    }
-    const handleDelete=(id:number)=>{
-        dispatch(deleteTodo(id))
-    }
-    //addnewtask function
-   const addTask=()=>{
-    dispatch(addTodo(input))
-   }
- 
-   const handleEdit=(todo:ITodo)=>{
-    setInput(todo.title)
-    setEditedId(todo.id)
-   }
-   
-    
-    const handleSubmit=(event:any)=>{
-        event.preventDefault()
-        if(editedId){
-            dispatch(updateTodo({
-                id:editedId,
-                title:input,
+  const todo = useSelector((state: RootState) => state.todo.todos);
 
-            }))
-            setEditedId(null)
-            setInput('')
-        }else{
-            addTask()
-            setInput('')
-        }
-        
-       
+  const [input, setInput] = useState("");
+
+  const [editedId, setEditedId] = useState<number | null>(null);
+
+  //inputhandler function
+
+  const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
+
+  //addnewtask function
+
+  const addTask = () => {
+    dispatch(addTodo(input));
+  };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+
+    if (editedId) {
+      dispatch(
+        updateTodo({
+          id: editedId,
+
+          title: input,
+        })
+      );
+
+      setEditedId(null);
+    } else {
+      addTask();
+
+      setInput("");
     }
+  };
   return (
-   <>
-   <form onSubmit={handleSubmit} className='w-96 border-2 mx-auto '>
-    <input type='text' placeholder='Add newTask.....' onChange={handleInput} value={input}></input>
-    <button type='submit'>{editedId?'Update':'Add'}</button>
-   
-   </form>
-   <div className='flex flex-col justify-center items-center'>
-    {  todo.map((item:ITodo)=>(
-      <div key={item.id} className='flex justify-around w-96 '>
-        <ul className={`${item.completed?'line-through':''}`}>
-       
-        <li>{item.title}</li>
-        <li>{item.completed}</li>
-      
-        </ul>
-        <input type='checkbox' onChange={()=>handleToogle(item.id)} checked={item.completed} ></input>
-         <button onClick={()=>handleDelete(item.id)}>Delete</button>
-         <button onClick={()=>handleEdit(item)}>Edit</button>
-        </div>
-         ))}
-        
-         </div>
+    <>
+      <form onSubmit={handleSubmit} className="w-96 border-2 mx-auto ">
+        <input
+          type="text"
+          placeholder="Add newTask....."
+          onChange={handleInput}
+          value={input}
+        ></input>
 
+        <button type="submit">Add</button>
+      </form>
 
-   </>
-  )
-}
+      <div className="flex flex-col justify-center items-center">
+        {todo.map((item: ITodo) => (
+          <div key={item.id} className="flex justify-around w-96">
+            <ul className="flex list-none ">
+              <li>{item.title}</li>
 
-export default form
+              <li>{item.completed}</li>
+            </ul>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default form;
